60f04f1d17da079254281eade938983169c7605e
Faraz Shaikh <farazshabbir@113AMBBLR-Faraz-Shabbir-Shaikh.local>
2021-08-22 22:44:27 +0530

    total wage calculation

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,19 +4,20 @@ public class EmployeeWage{
                 System.out.println("Welcome to Employee wage Computation program");
 		final int IS_FULL_TIME =1;
 		final int IS_PART_TIME =2;
-		int wageperhour=20,fulldayhour=0;
+		int wageperhour=20,fulldayhour=0,totworkinghours=0,totworkingdays=0;
 		int empwage=0,totalwage=0;
-		for(int i=1;i<=20;i++)
+		while(totworkinghours<100||totworkingdays<20)
 		{
 		int attendance=(int)Math.floor(Math.random()*10)%3;
 		switch (attendance) {
-			case  IS_FULL_TIME: fulldayhour=16;break;
-			case IS_PART_TIME: fulldayhour=8;break;
-			default: System.out.println("Employee is absent on day"+i);
+			case  IS_FULL_TIME: fulldayhour=16; totworkingdays+=1; break;
+			case IS_PART_TIME: fulldayhour=8; totworkingdays+=1; break;
+			default: fulldayhour=0; break;
 		}
 		empwage=fulldayhour*wageperhour ;
-		totalwage+=empwage;		
-		System.out.println("Employee wage for day"+i+" is "+empwage);
+		totalwage+=empwage;
+		totworkinghours+=fulldayhour;		
+		System.out.println("Employee wage for day is "+empwage);
 		}
 		System.out.println("Total wage of employee: "+totalwage);
 }
