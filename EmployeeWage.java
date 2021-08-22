bd1907e0cd048a2d3b28bc2817763be8b0481cc2
Faraz Shabbir Shaikh <55327028+FarazShaikhcse@users.noreply.github.com>
2021-08-22 21:39:36 +0530

    added daily wage calculation uc2

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,12 +3,19 @@ public class EmployeeWage{
 
                 System.out.println("Welcome to Employee wage Computation program");
 		final int IS_FULL_TIME =1;
-		
+		int wageperhour=20;
+		int fulldayhour=0,empwage=0;
 	
 		double attendance=Math.floor(Math.random()*10)%2;
 		if (attendance==IS_FULL_TIME)
+		{
 			System.out.println("Employee is present");
+			fulldayhour=8;
+		}
 		else
-			System.out.println("Employee is absent"); 
+			System.out.println("Employee is absent");
+		empwage=fulldayhour*wageperhour ;
+				
+		System.out.println("Employee wage= "+empwage);
 }
 }
