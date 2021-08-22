e62f72506dd7b89a757e54c2f89338f0e645ac01
Faraz Shaikh <farazshabbir@113AMBBLR-Faraz-Shabbir-Shaikh.local>
2021-08-22 21:19:39 +0530

    added attendance uc1

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,5 +2,13 @@ public class EmployeeWage{
         public static void main(String[] args) {
 
                 System.out.println("Welcome to Employee wage Computation program");
+		final int IS_FULL_TIME =1;
+		
+	
+		double attendance=Math.floor(Math.random()*10)%2;
+		if (attendance==IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent"); 
 }
 }
