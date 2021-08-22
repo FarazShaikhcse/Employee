55de383eb5392284c10165f4e8ff260b056e2319
Faraz Shaikh <farazshabbir@113AMBBLR-Faraz-Shabbir-Shaikh.local>
2021-08-22 22:11:48 +0530

    added part_time_uc3

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,19 +3,26 @@ public class EmployeeWage{
 
                 System.out.println("Welcome to Employee wage Computation program");
 		final int IS_FULL_TIME =1;
+		final int IS_PART_TIME =2;
 		int wageperhour=20;
 		int fulldayhour=0,empwage=0;
 	
-		double attendance=Math.floor(Math.random()*10)%2;
+		double attendance=Math.floor(Math.random()*10)%3;
 		if (attendance==IS_FULL_TIME)
 		{
-			System.out.println("Employee is present");
+			fulldayhour=16;
+		}
+		else if(attendance==IS_PART_TIME)
+		{
 			fulldayhour=8;
 		}
 		else
+		{
 			System.out.println("Employee is absent");
+		}
 		empwage=fulldayhour*wageperhour ;
 				
 		System.out.println("Employee wage= "+empwage);
 }
 }
+
